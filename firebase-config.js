e0f041c5a7f2422c0b4fc24ec25bd7dbefce630b
emmetthor <80567173+emmetthor@users.noreply.github.com>
2025-11-25 23:12:50 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPXOp6cpVhmkuETzFRwv5PV3sb6DUdlfc",
+  authDomain: "vocabulary-archive.firebaseapp.com",
+  projectId: "vocabulary-archive",
+  storageBucket: "vocabulary-archive.firebasestorage.app",
+  messagingSenderId: "1023222046624",
+  appId: "1:1023222046624:web:74c4ab5425311eab41cec5",
+  measurementId: "G-Q3HE1FFYFS"
+};
